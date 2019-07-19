95a5a71810d00c64dbb2ce375b135a80e4aee7ce
Amaris Merino <51328772+afmerino@users.noreply.github.com>
2019-07-19 15:45:00 -0400

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,4 +1,28 @@
 "use strict"
+var income = document.getElementById("income");
+
 
 var food = document.getElementById("food");
-var foodcost = food.value
+var foodcost = food.value;
+
+var gas = document.getElementbyId("gas");
+var gascost = gas.value;
+
+var rent = document.getElementById("rent");
+var rentcost = rent.value;
+
+var ins = document.getElementById("ins");
+var inscost = ins.value;
+
+var misc = document.getElementById("misc");
+var misccost = misc.value;
+
+console.log(income);
+console.log(foodcost);
+console.log(gascost);
+console.log(rentcost);
+console.log(inscost);
+console.log(misccost);
+
+
+
